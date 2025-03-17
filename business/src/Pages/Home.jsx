5269b025c15bef65348af8488b48881feb9d99b2
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2025-03-17 22:16:24 +0600

    some changed by me

--- business/src/Pages/Home.jsx
+++ business/src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../Components/Hero';
 import Service from '../Components/Service';
 import Companies from '../Components/Companies';
+import Plan from '../Components/Plan';
 const Home = () => {
   return (
     <div>
       <Hero/>
    <Service/>
    <Companies/>
+   <Plan/>
     </div>
   )
 }
